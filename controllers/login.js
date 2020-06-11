f283b0559b37e7a84cd4e1019337d5b26d6dd320
“Irving <Irving>
2020-06-10 19:02:12 -0500

    Final with credential key

--- controllers/login.js
+++ controllers/login.js
@@ -1,49 +1,42 @@
-const mysql = require('mysql');
-const dbConfig = require('../config/database.js');
 const cliente = require('../db_apis/cliente.js');
 const controller = {};
-const otpGenerator = require("otp-generator");
-const crypto       = require("crypto");
-const key          = "verysecretkey"; 
+const crypto = require("crypto");
+const key = "verysecretkey";
+
 
 controller.recovery_usuario = async function get(req, res, next) {
-    try {
-        const context = {};
+  try {
+    const context = {};
 
-            context.id = req.query.userId;
-            context.pass = req.query.userPass;
+    context.id = req.query.userId;
+    context.pass = req.query.userPass;
 
-            const rows = await cliente.find_cliente_userrecovery(context);
-            if (req.query.userId) {
-  
-                          res.status(200).json(rows[0]);
-        
-                    } else {
-  
-              res.status(200).json(rows);
-            }
-          } catch (err) {
-            next(err);
-          }
+    const rows = await cliente.find_cliente_userrecovery(context);
+      if (rows.length === 1) {
 
+        res.status(200).json(rows[0]);
 
+      }else{
+      res.status(200).json(rows);
+      }
+  } catch (err) {
+    next(err);
+  }
 }
 
 controller.create_code = async function post(req, res, next) {
   try {
 
-   const phone=req.body.celular;
-    const fullhash = await cliente.creacion_hash(phone);
+    const phone = req.body.celular;
+    const correo=req.body.correo;
+    const fullhash = await cliente.creacion_hash(phone,correo);
     res.status(200).json({
-     celular:'',
-     otp:'',
-     hash: fullhash,
-     estado: '',
+      hash: fullhash
     });
-  
+
   } catch (err) {
-      next(err);
-    }
+    next(err);
+  }
 
 
 }
@@ -52,34 +45,33 @@ controller.create_code = async function post(req, res, next) {
 controller.valid_code = async function post(req, res, next) {
   try {
 
- 
-      const phone=req.body.celular;
-      const otp=req.body.otp;
-      const hash=req.body.hash;
 
-      let [hashValue,expires]=hash.split(".");
-      let now = Date.now();
-      if(now>parseInt(expires)) {
-        res.status(200).json({
-          estado: 'timeout'
-        });
-      return false;
-      }
-      let data  = `${phone}.${otp}.${expires}`;
-      let newCalculatedHash = crypto.createHmac("sha256",key).update(data).digest("hex");
-      if(newCalculatedHash === hashValue){
-        res.status(200).json({
-          estado: 'correcto'
-        });
-          return true;
-      } 
+    const phone = req.body.celular;
+    const otp = req.body.otp;
+    const hash = req.body.hash;
+    let [hashValue, expires] = hash.split(".");
+    let now = Date.now();
+    if (now > parseInt(expires)) {
       res.status(200).json({
-        estado: 'incorrecto'
+        estado: 'timeout'
       });
       return false;
-  } catch (err) {
-      next(err);
     }
+    let data = `${phone}.${otp}.${expires}`;
+    let newCalculatedHash = crypto.createHmac("sha256", key).update(data).digest("hex");
+    if (newCalculatedHash === hashValue) {
+      res.status(200).json({
+        estado: 'valido'
+      });
+      return true;
+    }
+    res.status(200).json({
+      estado: 'novalido'
+    });
+    return false;
+  } catch (err) {
+    next(err);
+  }
 
 
 }
--- db_apis/cliente.js
+++ db_apis/cliente.js
@@ -3,6 +3,17 @@ const mysql = require('mysql');
 const otpGenerator = require("otp-generator");
 const crypto = require("crypto");
 const key = "verysecretkey";
+const nodemailer = require('nodemailer');
+const myCredentials = require('schluessel');
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: myCredentials.correo.username,
+    pass: myCredentials.correo.password 
+  }
+});
+
+
 
 
 const calif_estudsQuery =
@@ -26,11 +37,24 @@ async function find_cliente_userrecovery(context) {
 
 }
 
+function send_mail(correo,otp){
+  var mailOptions = {
+    from: myCredentials.correo.username,
+    to: correo,
+    subject: 'Sending Email from Node.js',
+    text: 'Codigo OTP:'+otp
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
+
 function send_code(phone, otp) {
 
-
-  // const phone = req.body.celular || 'none'
-  // const code = req.body.codigo || 'none'
   const codeValid = otp
   console.log("enviando mediante twilio", phone);
   if (phone == 'none')
@@ -45,15 +69,13 @@ function send_code(phone, otp) {
     // Download the helper library from https://www.twilio.com/docs/node/install
     // Your Account Sid and Auth Token from twilio.com/console
     // DANGER! This is insecure. See http://twil.io/secure
-
-
-   const accountSid = 'ACa26baa654624f2414d7fe7f7d824a3a8';
-   const authToken = 'f05ae72657ee5e13d11fa506d822243e';
-    const client = require('twilio')(accountSid, authToken);
+   //const accountSid = 'ACa26baa654624f2414d7fe7f7d824a3a8';
+   //const authToken = 'f05ae72657ee5e13d11fa506d822243e';
+    const client = require('twilio')(myCredentials.twilio.accountSid,myCredentials.twilio.authToken);
     client.messages
       .create({
         body: codeValid + ' este es su código de verificación',
-        from: '+19032318201',
+        from: myCredentials.twilio.myphone,
         to: '+507' + phone
       })
       .then(message => {
@@ -68,7 +90,7 @@ function send_code(phone, otp) {
 }
 
 
-async function creacion_hash(phone) {
+async function creacion_hash(phone,correo) {
   const otp = otpGenerator.generate(6, { alphabets: false, upperCase: false, specialChars: false });
   console.log("****CODIGO OTP****", otp);
   const ttl = 3 * 60 * 1000; //3 Minutes in miliseconds
@@ -77,6 +99,7 @@ async function creacion_hash(phone) {
   const hash = crypto.createHmac("sha256", key).update(data).digest("hex");
   const fullHash = `${hash}.${expires}`;
   //send_code(phone,otp)
+  //send_mail(correo,otp);
   return fullHash;
 }
 
